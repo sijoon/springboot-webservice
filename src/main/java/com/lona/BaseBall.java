f8ee2045ecfddb5d190930edbd48812f3887962c
sijoon <sijoon.seong@gmail.com>
2024-05-21 14:57:25 +0900

    HelloControllerTest 수정 적용

--- src/main/java/com/lona/BaseBall.java
+++ src/main/java/com/lona/BaseBall.java
@@ -1,8 +1,9 @@
 package com.lona;
 
+import java.util.Scanner;
 public class BaseBall {
     //     스트라이크 카운트 메서드
-    public static int strike(String resultNum, String tryNum, int countStrike) {
+    private static int strike(String resultNum, String tryNum, int countStrike) {
         for (int i = 0; i < resultNum.length(); i++) {
             if (resultNum.charAt(i) == tryNum.charAt(i)) {
                 countStrike += 1;
@@ -12,20 +13,54 @@ public class BaseBall {
     }
 
     //    볼 카운트 메서드
-    public static int ball(String resultNum, String tryNum, int countBall) {
+    private static int ball(String resultNum, String tryNum, int countBall) {
 
-        String strBallCnt = "";
-        int intBallCnt = 0;
+        StringBuilder strBallCnt = new StringBuilder();
+        int intBallCnt;
 
         for (int i = 0; i < resultNum.length(); i++) {
 
             if (resultNum.indexOf(tryNum.charAt(i)) >= 0 && resultNum.charAt(i) != tryNum.charAt(i)) {
-                strBallCnt += Integer.toString(i);
+                strBallCnt.append(Integer.toString(i));
                 intBallCnt = strBallCnt.length();
                 countBall = intBallCnt;
             }
         }
         return countBall;
+    }
+
+    public void playRun(){
+        int[] nums = new int[3];
+        int letter = nums.length;
+
+        int countStrike = 0;
+        int countBall = 0;
+
+        String resultNum = "123";
+        String tryNum;
+
+        /* 게임 시작 */
+        for (int i = 0; ; i++) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Given 123, Input ");
+            tryNum = sc.next();
+
+            int bcnt = ball(resultNum, tryNum, countBall);
+            int scnt = strike(resultNum, tryNum, countStrike);
+
+            if (tryNum.equals(resultNum)) {
+                System.out.println(letter + "S");
+            } else if (bcnt > 0 && scnt > 0) {
+                System.out.println(scnt + "S" + bcnt + "B");
+            } else if (bcnt > 0) {
+                System.out.println(bcnt + "B");
+            } else if (scnt > 0) {
+                System.out.println(scnt + "S");
+            } else {
+                System.out.println("(null)");
+            }
+        }
+
     }
 
 }
