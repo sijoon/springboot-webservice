a89df840ad07688cd4ec8d7f9eec85a5877353e3
sijoon <sijoon.seong@gmail.com>
2024-05-28 13:12:36 +0900

    PostsRepositoryTest.java 실습 작성

---
+++ src/main/java/com/lona/domain/post/Posts.java
@@ -0,0 +1,39 @@
+package com.lona.domain.post;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
