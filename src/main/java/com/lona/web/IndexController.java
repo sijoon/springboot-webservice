06bdbbb669338dac792bb102e6f0fd46f50cfa54
sijoon <sijoon.seong@gmail.com>
2024-06-04 14:21:58 +0900

    머스테치

---
+++ src/main/java/com/lona/web/IndexController.java
@@ -0,0 +1,13 @@
+package com.lona.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
